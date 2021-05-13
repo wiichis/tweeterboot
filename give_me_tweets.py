98c3686f767af3fe8261d4009f3e3b422958e1ec
Wilson Montenegro <wilmontenegro@me.com>
2021-05-12 23:38:55 -0500

    Poniendo tiempo para ejecutar la recolección.

--- give_me_tweets.py
+++ give_me_tweets.py
@@ -1,5 +1,6 @@
 import credentials
 import tweepy
+import schedule
 
 auth = tweepy.OAuthHandler(credentials.API_Key, credentials.API_Secret_Key)
 auth.set_access_token(credentials.Acces_token, credentials.Acces_token_secret)
@@ -11,17 +12,28 @@ api = tweepy.API(auth)
 # for tweet in public_tweets:
 #     print(f'{tweet.user.screen_name}:\n{tweet.text}\n{"*"*60}')
 
-id = None
-count = 0
-while count <= 3000:
-    tweets = api.search(q='#CoheteChino', lang='es', tweet_mode='extended', max_id=id)
-    for tweet in tweets:
-        if tweet.full_text.startswith('RT'):
+def get_tweets():
+    id = None
+    count = 0
+    while count <= 1000:
+        tweets = api.search(q='#Bitcoin', lang='es', tweet_mode='extended', max_id=id)
+        for tweet in tweets:
+            if tweet.full_text.startswith('RT'):
+                count += 1
+                continue
+            f = open('./data_tweets.txt', 'a', encoding='utf-8')
+            f.write(tweet.full_text + '\n')
+            f.close
             count += 1
-            continue
-        f = open('./data_tweets.txt', 'a', encoding='utf-8')
-        f.write(tweet.full_text + '\n')
-        f.close
-        count += 1
-    id = tweet.id
-    print(count)
+        id = tweet.id
+        print(count)
+    
+def run():
+    get_tweets()
+
+if __name__=='__main__':
+    schedule.every(6).minutes.do(run)
+    
+
+    while True:
+        schedule.run_pending()
